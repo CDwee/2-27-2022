cdb350f42be993e5d54c744b800b07ac38304475
CDwee <96454637+CDwee@users.noreply.github.com>
2022-02-27 15:43:41 -0600

    Update script.js

--- script.js
+++ script.js
@@ -60,3 +60,53 @@ const calcAgeArrow = birthYear => {
 calcAgeArrow(1980);
 
 // Ended my first hour at 12:24 2-27-2022
+
+// Started my second hour at 2:43 2-27-2022
+
+// var firstName = 'Matilda';
+
+const jonas = {
+    firstName: 'Jonas',
+    year: 1991,
+    calcAge: function() {
+        // console.log(this);
+        console.log(2037 - this.year);
+
+        // Solution 1 
+        // const self = this; // can be called self or that
+        // const isMillenial = function() {
+        //     console.log(self);
+        //     console.log(self.year >= 1981 && self.year <= 1996);
+        // };
+
+        // Solution 2 
+        const isMillenial = () => {
+            console.log(this);
+            console.log(this.year >= 1981 && this.year <= 1996);
+        };
+        isMillenial();
+    },
+
+    greet: () => {
+        console.log(this);
+        console.log(`Hey ${this.firstName}`);
+    },
+};
+jonas.greet();
+jonas.calcAge();
+
+// argument keyword
+const addExpr = function(a, b) {
+    console.log(arguments);
+    return a + b;
+};
+addExpr(2, 5);
+addExpr(2, 5, 8, 12);
+
+var addArrow = (a, b) => {
+    console.log(arguments);
+    return a + b
+};
+addArrow(2, 5, 8);
+
+// Ended my second hour at 3:43 2-27-2022
