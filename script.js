6e01bc5b1c9f6137a0c8c9372c358640f22d0edb
CDwee <96454637+CDwee@users.noreply.github.com>
2022-02-27 19:52:45 -0600

    Update script.js

--- script.js
+++ script.js
@@ -110,3 +110,42 @@ var addArrow = (a, b) => {
 addArrow(2, 5, 8);
 
 // Ended my second hour at 3:43 2-27-2022
+
+// Started my third hour at 6:49 2-27-2022
+
+// Primitive types
+let lastName = 'Williams';
+let oldLastName = lastName;
+lastName = 'Davis';
+console.log(lastName, oldLastName);
+
+// Reference types
+const jessica = {
+    firstName: 'Jessica',
+    lastName: 'Williams',
+    age: 27,
+};
+const marriedJessica = jessica;
+marriedJessica.lastName = 'Davis';
+console.log('Before marriage:', jessica);
+console.log('After marriage: ', marriedJessica);
+
+// marriedJessica = {};  
+// Copying objects
+const jessica2 = {
+    firstName: 'Jessica',
+    lastName: 'Williams',
+    age: 27,
+    family: ['Alice', 'Bob']
+};
+
+const jessicaCopy = Object.assign({}, jessica2);
+jessicaCopy.lastName = 'Davis';
+
+jessicaCopy.family.push('Mary');
+jessicaCopy.family.push('John');
+
+console.log('Before marriage:', jessica2);
+console.log('After marriage: ', jessicaCopy);  
+
+// Ended my second hour at 7:52 2-27-2022
